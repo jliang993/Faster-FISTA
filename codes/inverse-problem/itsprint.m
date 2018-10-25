f2b388fe432f7623dc67578fdfeab6b192e1361f
Jingwei Liang <9018003+jliang993@users.noreply.github.com>
2018-10-25 19:19:20 +0100

    fix problems

--- codes/inverse-problem/itsprint.m
+++
@@ -1,8 +0,0 @@
-function itsprint(str, k)
-%ITSPRINT displays the iteration information
-
-if(k==1)
-    fprintf(str);
-end
-
-fprintf('%s%s', char(8*ones(1,length(str))), str);
--- codes/pcp/grad_pcp.m
+++
@@ -1,9 +0,0 @@
-function g = grad_pcp(x, theta, beta)
-% gradient of Moreau Envelope
-% fx = M - x
-% just for NON-negative Robust PCA
-
-x_ = max( wthresh(x, 's', theta), 0 );
-% x_ = max(x_, 0);
-
-g = -(x - x_)/beta;
